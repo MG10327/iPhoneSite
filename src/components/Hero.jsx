3cc33847e5d8b0d77be189da5aa0c0baa7e1e283
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-10-03 19:15:29 -0400

    hero video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import gsap from 'gsap'
 import {useGSAP} from '@gsap/react'
+import {heroVideo, smallHeroVideo} from '../utils'
 
 const Hero = () => {
+
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
     useGSAP(() => {
         gsap.to('#hero', {opacity: 1, delay: 1.5 })
     })
 
   return (
-    <section className='w-full nav-height bg-black relative'>
+    <section className='w-full nav-height bg-black relative grid'>
         <div className="h-5/6 w-full flex-center flex-col">
             <p id="hero" className='hero-title'>iPhone 15 Pro</p>
+        </div>
+        <div className='md:w-10/12 w-9/12 place-center'>
+            <video autoPlay muted playsInline>
+                <source src={videoSrc} type='video/mp4' />
+            </video>
         </div>
     </section>
   )
