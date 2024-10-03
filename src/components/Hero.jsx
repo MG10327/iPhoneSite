d252c75d003c73e1109385b97ff1073154d65baf
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-10-03 19:38:04 -0400

    add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import gsap from 'gsap'
 import {useGSAP} from '@gsap/react'
 import {heroVideo, smallHeroVideo} from '../utils'
@@ -7,8 +7,28 @@ const Hero = () => {
 
     const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
+    const handleVideoSrcSet = () => {
+        if (window.innerWidth < 760) {
+            setVideoSrc(smallHeroVideo)
+        } else {
+            setVideoSrc(heroVideo)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoSrcSet)
+        return () => {
+            window.removeEventListener('resize', handleVideoSrcSet)
+        }
+    }, [])
+
     useGSAP(() => {
-        gsap.to('#hero', {opacity: 1, delay: 1.5 })
+        gsap.to('#hero', {opacity: 1, delay: 2 })
+    })
+
+
+    useGSAP(() => {
+        gsap.to('#cta', {opacity: 1, delay: 2, y: -50 })
     })
 
   return (
@@ -16,10 +36,16 @@ const Hero = () => {
         <div className="h-5/6 w-full flex-center flex-col">
             <p id="hero" className='hero-title'>iPhone 15 Pro</p>
         </div>
-        <div className='md:w-10/12 w-9/12 place-center'>
-            <video autoPlay muted playsInline>
+        <div className='md:w-10/12 w-9/12 place-self-center'>
+            <video autoPlay muted playsInline={true} key={videoSrc}>
                 <source src={videoSrc} type='video/mp4' />
             </video>
+        </div>
+
+        <div id='cta'
+        className='flex flex-col items-center opacity-0 translate-y-20'>
+            <a href="#highlights" className='btn'>Buy</a>
+            <p className='font-normal text-xl'>From $199/month or $999</p>
         </div>
     </section>
   )
