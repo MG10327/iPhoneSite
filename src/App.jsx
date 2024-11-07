abae522deb56bcc71fd87646e9d5d4db871c62a0
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-07 10:29:35 -0500

    basic sentry setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
+import * as Sentry from '@sentry/react'
 
 const App = () => {
+
   return (
     <main className='bg-black'>
       <Navbar></Navbar>
@@ -15,4 +17,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
