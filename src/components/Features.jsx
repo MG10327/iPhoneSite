5b1119588fb3235d50f7a5d9887cd48b5115955c
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-07 11:19:21 -0500

    wrap up features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,19 +2,34 @@ import { useGSAP } from '@gsap/react'
 import React, {useRef} from 'react'
 import { animateWithGsap } from '../utils/animations'
 import { explore1Img, explore2Img, exploreVideo } from '../utils'
+import gsap from 'gsap'
 
 const Features = () => {
     const videoRef = useRef()
 
     useGSAP(() => {
-        animateWithGsap('features_title', {y: 0, opacity: 1})
-        animateWithGsap('.g_grow',
-            {scale: 1, opacity: 1,}
+        gsap.to('#exploreVideo', {
+          scrollTrigger: {
+            trigger: '#exploreVideo',
+            toggleActions: 'play pause reverse restart',
+            start: '-10% bottom',
+          },
+          onComplete: () => {
+            videoRef.current.play();
+          }
+        })
+    
+        animateWithGsap('#features_title', { y:0, opacity:1})
+        animateWithGsap(
+          '.g_grow',
+          { scale: 1, opacity: 1, ease: 'power1' },
+          { scrub: 5.5 }
+        );
+        animateWithGsap(
+          '.g_text',
+          {y:0, opacity: 1,ease: 'power2.inOut',duration: 1}
         )
-        animateWithGsap('.g_text',
-            {y: 0, opacity: 1, ease: 'power2.inOut', duration: 1}
-        )
-    })
+      }, []);
 
   return (
     <section className='h-full common-padding bg-zinc relative overflow-hidden'>
@@ -58,7 +73,7 @@ const Features = () => {
                                 </p>
                             </div>
                             <div className="flex-1 flex-center">
-                                <p>Titanium has one of the bst strength-to-weight ratios of any metal, making these our {''}
+                                <p className='feature-text g_text'>Titanium has one of the bst strength-to-weight ratios of any metal, making these our {''}
                                     <span className='text-white'>
                                         lightest Pro models ever.
                                     </span>,
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -6,10 +6,12 @@ gsap.registerPlugin(ScrollTrigger)
 export const animateWithGsap = (target, animationProps, scrollProps) => {
     gsap.to(target, {
         ...animationProps,
-        trigger: target,
-        toggleActions: 'restart reverse restart reverse ',
-        start: "top 85%",
-        ...scrollProps
+        scrollTrigger: {
+            trigger: target,
+            toggleActions: 'restart reverse restart reverse ',
+            start: "top 85%",
+            ...scrollProps
+        }
     })
 }
 
